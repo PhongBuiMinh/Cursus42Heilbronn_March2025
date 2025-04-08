10df3dc0010a5b4745443041783e0da29e15f6d1
Fonh Bui Min <fbui-min@3-D-4.42heilbronn.de>
2025-04-08 17:56:36 +0200

    split files; fixed empty string outut; fied 0 pointer output

--- Circle01/printf/ft_printf.c
+++ Circle01/printf/ft_printf.c
@@ -3,122 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phong <phong@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fbui-min <fbui-min@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/07 12:27:04 by phong             #+#    #+#             */
-/*   Updated: 2025/04/07 13:41:11 by phong            ###   ########.fr       */
+/*   Created: 2025/04/01 19:25:54 by fbui-min          #+#    #+#             */
+/*   Updated: 2025/04/08 17:53:40 by fbui-min         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-bool	ft_put_char(char c, size_t *count)
+bool	determine_format(va_list params, char c, size_t *count)
 {
-	if (write(1, &c, 1) < 0)
-		return (false);
-	(*count)++;
-	return (true);
-}
+	bool	status;
 
-bool	ft_put_udec(unsigned int n, size_t *count)
-{
-	if (n >= 10)
-		if (!ft_put_udec(n / 10, count))
-			return (false);
-	return (ft_put_char((n % 10) + 48, count));
-}
-
-bool	ft_put_str(char *str, size_t *count)
-{
-	if (!str)
-	{
-		write(1, "(null)", 6);
-		return (false);
-	}
-	while (*str)
-	{
-		if (!ft_put_char(*str, count))
-			return (false);
-		str++;
-	}
-	return (true);
-}
-
-bool	ft_put_dec(int n, size_t *count)
-{
-	long	nbr;
-
-	nbr = n;
-	if (nbr < 0)
-	{
-		if (!ft_put_char('-', count))
-			return (false);
-		nbr = -nbr;
-	}
-	if (nbr >= 10)
-		if (!ft_put_dec(nbr / 10, count))
-			return (false);
-	return (ft_put_char((nbr % 10) + 48, count));
-}
-
-bool	ft_put_hex(unsigned int nbr, size_t *count, char cap)
-{
-	char	digit;
-
-	if (nbr >= 16)
-		if (!ft_put_hex(nbr / 16, count, cap))
-			return (false);
-	digit = nbr % 16;
-	if (digit < 10)
-		digit += 48;
-	else if (cap == 'X')
-		digit += 65 - 10;
-	else if (cap == 'x')
-		digit += 97 - 10;
-	return (ft_put_char(digit, count));
-}
-
-bool	ft_put_ptr(void *address, size_t *count, bool is_first_call)
-{
-	char			digit;
-	unsigned long	nbr;
-
-	nbr = (unsigned long)address;
-	if (is_first_call)
-	{
-		if (address == 0 || !ft_put_str("0x", count))
-			return (false);
-	}
-	if (nbr >= 16)
-		if (!ft_put_ptr((void *)(nbr / 16), count, false))
-			return (false);
-	digit = nbr % 16;
-	if (digit < 10)
-		digit += 48;
-	else
-		digit += 97 - 10;
-	return (ft_put_char(digit, count));
-}
-
-bool	check(va_list params, char c, size_t *count)
-{
-	bool	check;
-
+	status = true;
 	if (c == 'c')
-		check = ft_put_char(va_arg(params, int), count);
+		status = ft_put_char(va_arg(params, int), count);
 	else if (c == 's')
-		check = ft_put_str(va_arg(params, char *), count);
+		status = ft_put_str(va_arg(params, char *), count);
 	else if (c == 'p')
-		check = ft_put_ptr(va_arg(params, void *), count, true);
+		status = ft_put_ptr(va_arg(params, void *), count, true);
 	else if (c == 'd' || c == 'i')
-		check = ft_put_dec(va_arg(params, int), count);
+		status = ft_put_dec(va_arg(params, int), count);
 	else if (c == 'u')
-		check = ft_put_udec(va_arg(params, unsigned int), count);
+		status = ft_put_udec(va_arg(params, unsigned int), count);
 	else if (c == 'x' || c == 'X')
-		check = ft_put_hex(va_arg(params, int), count, c);
+		status = ft_put_hex(va_arg(params, int), count, c);
 	else if (c == '%')
-		check = ft_put_char('%', count);
-	return (check);
+		status = ft_put_char('%', count);
+	return (status);
 }
 
 int	ft_printf(const char *format, ...)
@@ -128,13 +41,13 @@ int	ft_printf(const char *format, ...)
 
 	count = 0;
 	va_start(params, format);
-	if (!(*format))
+	if (!format)
 		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			if (!check(params, *(format + 1), &count))
+			if (!determine_format(params, *(format + 1), &count))
 				return (va_end(params), -1);
 			format += 2;
 			continue ;
@@ -147,8 +60,10 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-int	main(void)
+/*int	main(void)
 {
+	int	check = ft_printf("%s", (char *)NULL);
+	ft_printf("%i\n", check);
 	int	value = 0;
 	ft_printf("=========================\n");
 	ft_printf("My function\n");
@@ -197,4 +112,4 @@ int	main(void)
 	ft_printf("=========================\n");
 	printf("%x\n", -1);
 	return (0);
-}
+}*/
