4504378e839782fc598542f01ccede51c0c34cd7
Fonh Bui Min <fbui-min@1-A-28.42heilbronn.de>
2025-08-24 17:59:20 +0200

    script running on Mac and linux

--- Circle02/push_swap/input.sh
+++ Circle02/push_swap/input.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 
-echo -e "Enter amount of random numbers"
-./push_swap "$@"
-
+OS=$(uname)
+NAME=./push_swap
 MIN=-300
 MAX=300
 
+chmod 777 input.sh $NAME
+
+if [ "$OS" == "Darwin" ]; then
+	RANDOMIZER="sort -R"
+	CHECKER=checker_Mac
+elif [ "$OS" == "Windows" ]; then
+	RANDOMIZER=shuf
+	CHECKER=checker_linux
+fi
+
+ARG=$(seq $MIN $MAX | $RANDOMIZER | head -n $1 | paste -sd' ' -)
+
+echo -e "\n======= Push_Swap Tester =======\n"
+
+echo -e "File information"
+file $NAME
+
+echo -e "\nPassing set of $1 numbers: $ARG"
+
+echo -e "\nCounting required operations"
+$NAME $ARG | wc -l | tr -d ' '
+
+echo -e "\nTesting with checker program"
+$NAME $ARG | ./tests/$CHECKER $ARG
+
+# case $1 in
+#   5)
+# 	ARG=$(seq $MIN $MAX | sort -R | head -n 5 | paste -sd' ' -)
+# 	;;
+#   100)
+# 	ARG=$(seq $MIN $MAX | shuf | head -n 100 | paste -sd' ' -)
+# 	;;
+#   500)
+# 	ARG=$(seq $MIN $MAX | shuf | head -n 500 | paste -sd' ' -)
+# 	;;
+#   *)
+# 	echo "Usage: $0 [5|100|500]"
+# 	exit 1
+# 	;;
+# esac
+
 # ARG=""
-
-case $1 in
-  5)
-	ARG=$(seq $MIN $MAX | shuf | head -n 5 | paste -sd' ' -)
-	;;
-  100)
-	ARG=$(seq $MIN $MAX | shuf | head -n 100 | paste -sd' ' -)
-	;;
-  500)
-	ARG=$(seq $MIN $MAX | shuf | head -n 500 | paste -sd' ' -)
-	;;
-esac
-
-echo -e "\n======= Push_Swap Tester ======="
-echo -e "Passing set of numbers: $ARG"
-echo -e "Counting required operations"
-./push_swap $ARG | wc -l
-echo -e "Testing with checker program"
-./push_swap $ARG | ./tests/checker_linux $ARG
+# $NAME "$@"
 
 # # Generate N unique random numbers between MIN and MAX
 # N=100           # Number of random numbers you want
